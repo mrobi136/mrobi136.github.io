5674bbdf9d8c4d1905799d53833d332f2e70b63b
mrobi136 <dadmike64@gmail.com>
2024-07-11 14:02:21 -0400

    Create basicMap.js

---
+++ basicMap.js
@@ -0,0 +1,12 @@
+console.log("JS File was loaded");
+
+// 35.217665 N, 80.831473 W -> 35.217665, -80.831473
+
+let map = L.map("map", {
+  center: [35.217665, -80.831473],
+  zoom: 8
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
