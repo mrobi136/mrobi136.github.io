4c04c337411ca1c56b64420d847488a48a11e4d6
mrobi136 <dadmike64@gmail.com>
2024-07-16 14:06:34 -0400

    Update florida.js

--- florida.js
+++ florida.js
@@ -4,6 +4,8 @@ let map = L.map("map", {
   zoom:15
 });
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
