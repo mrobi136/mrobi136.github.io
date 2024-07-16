06431e7d941998db54d81060524f6de4d60d3cb6
mrobi136 <dadmike64@gmail.com>
2024-07-16 14:21:05 -0400

    Update florida.js

--- florida.js
+++ florida.js
@@ -5,8 +5,8 @@ let map = L.map("map", {
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    max/zoom:19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 // everglades: coordinates: 25.2866° N, 80.8987° W-->25.2866, -80.8987/
