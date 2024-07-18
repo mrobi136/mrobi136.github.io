8a016c0866d9ad605dc9157bb8ec9642a69cfbc2
mrobi136 <dadmike64@gmail.com>
2024-07-18 14:26:40 -0400

    Update earthquakes.js

--- earthquakes.js
+++ earthquakes.js
@@ -4,5 +4,15 @@ d3.json(url).then(
 function(data)
   {
     console.log(data);
-  }
+
+    let map = L.map("map", {
+      center: [37.99, -95.71],
+      zoom: 5
+  });
+
+var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+	maxZoom: 20,
+	attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+}).addTo(map);
+    
 );
